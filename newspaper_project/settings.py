a5961f4700c5a72c839b63eae42f5f64b85fac0a
musa <musa.thakur@gmail.com>
2021-07-14 23:20:17 +0600

    Bootstrap

--- newspaper_project/settings.py
+++ newspaper_project/settings.py
@@ -31,7 +31,13 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    # Local
     'users.apps.UsersConfig',
+    'pages.apps.PagesConfig',
+
+    # 3rd Party
+    'crispy_forms',
+
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -130,4 +136,5 @@ AUTH_USER_MODEL = 'users.CustomUser'
 
 LOGIN_REDIRECT_URL = 'home'
 LOGOUT_REDIRECT_URL = 'home'
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
